761e045ff702b5f8077f47ca486341b95991ba85
KatiaN14 <e.novakova@edu.uah.es>
2025-02-23 20:46:04 +0100

    dokonceni prvni funkcni verze

--- studiumspanelsko/script.js
+++ studiumspanelsko/script.js
@@ -29,7 +29,11 @@ async function updateContent(lang) {
     document.querySelectorAll('[data-i18n]').forEach(element => {
         const key = element.getAttribute('data-i18n');
         if (langData[key]) {
+            if(langData[key].includes('<')) {
+                element.innerHTML = langData[key];
+            }else{
             element.textContent = langData[key];
+            }
         }
     });
 }
@@ -57,9 +61,9 @@ async function loadPage(page) {
                 <h1 data-i18n="welcome_message">Welcome to Our Website!</h1>
                 <p data-i18n="paragraph1">This is the first paragraph of text.</p>
                 <p data-i18n="paragraph2">This is the second paragraph of text.</p>
-                <form id="emailForm">
+                <!--<form id="emailForm">
                     <input type="email" id="email" placeholder="Mail" required>
-                    <textarea id="question" placeholder="..." required></textarea>
+                    <textarea id="question" placeholder="ZATIM FORMULAR NEFUNGUJE, POUZIJ WHATSAPP" required></textarea>
                     <select id="preferredLanguage">
                         <option value="cz">čeština</option>
                         <option value="sk">slovenčina</option>
@@ -67,7 +71,8 @@ async function loadPage(page) {
                         <option value="en">english</option>
                     </select>
                     <button type="submit">&#128232</button>
-                </form>
+                </form>-->
+                <h2><a href="https://chat.whatsapp.com/JTJGUWzaxkJLBEkfYmL7s0">WhatsApp Group link</a></h2>
                 <h2 data-i18n="contact_us">Contact Us</h2>
                 <div class="team">
                     <div class="member">
@@ -89,7 +94,7 @@ async function loadPage(page) {
                         <p data-i18n="spain2">2023</p>
                     </div>
                     <div class="member">
-                        <img src="assets/person3.jpg" alt="Person 3">
+                        <img src="assets/david.jpg" alt="Person 3">
                         <p data-i18n="name3">Highschool: XYZ, University: ABC, Degree: BSc</p>
                         <p data-i18n="tel3">tel:</p>
                         <p data-i18n="school3">highschool</p>
@@ -110,22 +115,26 @@ async function loadPage(page) {
             `;
             break;
 
-        case 'todo-list':
+        case 'todo':
             mainContent.innerHTML = `
-                <h1 data-i18n="todo_list">Todo List</h1>
+                <h1 data-i18n="todo_list">To-do List</h1>
                 <ul>
                     <li data-i18n="todo1">Task 1</li>
                     <li data-i18n="todo2">Task 2</li>
                     <li data-i18n="todo3">Task 3</li>
+                    <li data-i18n="todo4">Task 4</li>
+                    <li data-i18n="todo5">Task 5</li>
+                    <li data-i18n="todo6">Task 6</li>
                 </ul>
             `;
             break;
-        case 'useful-info':
+        case 'alcala':
             mainContent.innerHTML = `
                 <h1 data-i18n="infoh1">H1</h1>
                     <p data-i18n="infop1">p1</p>
                 <h2 data-i18n="info2kolik">H2</h2>
                     <p data-i18n="infop2">p2</p>
+                <img src="assets/uah.jpg" alt="Alcala" class="alcala-img">
                 <h2 data-i18n="info3sance">H3</h2>
                     <p data-i18n="infop3">p3</p>
                 <h2 data-i18n="info4kdy">H4</h2>
@@ -138,6 +147,10 @@ async function loadPage(page) {
                 <h2 data-i18n="info5jakvypada">H5</h2>
                     <p data-i18n="infop5">p5</p>
                 <h2 data-i18n="info6pozor">H6</h2>
+                <figure>
+                <img src="assets/eps.jpg" alt="Campus cientifico tecnologico UAH" class="alcala-img">
+                <figcaption>Campus científico-tecnológico UAH</figcaption>
+                </figure>   
                     <p data-i18n="infop61">p6</p>
                     <p data-i18n="infop62">p6</p>
                     <p data-i18n="infop63">p6</p>
@@ -149,6 +162,10 @@ async function loadPage(page) {
                     <p data-i18n="infop69">p6</p>
                     <p data-i18n="infop610">p6</p>
                 <h2 data-i18n="info7zarucuje">H7</h2>
+                <figure>
+                <img src="assets/crusa.jpg" alt="Residencia de estudiantes CRUSA" class="alcala-img">
+                <figcaption>CRUSA - Residencia de estudiantes</figcaption>
+                </figure>
                     <p data-i18n="infop7">p7</p>
                 <ul>
                     <li data-i18n="info7a1">a1</li>
@@ -161,16 +178,35 @@ async function loadPage(page) {
                 <p data-i18n="infokonec">p9</p>
             `;
             break;
-        case 'contact':
+        case 'vocab':
             mainContent.innerHTML = `
-                <h1 data-i18n="contact-h1">Contact Us</h1>
-                <p data-i18n="contact-paragraph">This is the contact page.</p>
+                <h1 data-i18n="vocabh1">Contact Us</h1>
+                <p data-i18n="vocabinfo">This is the contact page.</p>
+                <ul>
+                    <li data-i18n="vocab1">Pojem1</li>
+                    <li data-i18n="vocab2">Pojem2</li>
+                    <li data-i18n="vocab3">Pojem3</li>
+                    <li data-i18n="vocab4">Pojem4</li>
+                    <li data-i18n="vocab5">Pojem5</li>
+                    <li data-i18n="vocab6">Pojem6</li>
+                    <li data-i18n="vocab7">Pojem7</li>
+                    <li data-i18n="vocab8">Pojem8</li>
+                    <li data-i18n="vocab9">Pojem9</li>
+                    <li data-i18n="vocab10">Pojem10</li>
+                </ul>
             `;
             break;
-        case 'faq':
+        case 'studyspain':
             mainContent.innerHTML = `
-                <h1 data-i18n="faq-h1">FAQ</h1>
-                <p data-i18n="faq-paragraph">This is the FAQ page.</p>
+                <h1 data-i18n="studyh1">H1</h1>
+                <p data-i18n="studyp1">info</p>
+                <p data-i18n="studyp12">H2</p>
+                <p data-i18n="studyp13">H3</p>
+                <p data-i18n="studyp2">H2</p>
+                <ul>
+                    <li><a href="https://escuelapce.com/estudiar-en-espana-siendo-extranjero/">Escuela PCE - estudiar en España siendo extranjero</a> - jako student z bilingvního gymnázia, nebude potřeba udělat všechno, co tady píšou, ale jsou tam užitečné informace</li>
+                    <li><a href="https://www.educaweb.com/contenidos/educativos/selectividad/notas-corte/">Notas de corte</a></li>
+                </ul>
             `;
             break;
 
@@ -208,6 +244,9 @@ function handleSidebarLinkClick(event) {
     event.preventDefault();
     const page = this.getAttribute('data-page');
     loadPage(page);
+    if(window.innerWidth < 768){
+        toggleMenu();
+    }
 }
 
 // Handle logo click (redirect to home page)
@@ -250,4 +289,19 @@ function toggleMenu() {
     } else {
         sidebar.style.display = "block";
     }
+}
+
+
+// Guardar preferencias de idioma
+
+function saveLanguage() {
+    var language = document.querySelector(".language-switcher").value;
+    localStorage.setItem('language', language);
+    changeLanguage(language);
+}
+
+function getLanguage() {
+    var language = localStorage.getItem('language');
+    document.querySelector(".language-switcher").value = language;
+    changeLanguage(language);
 }
